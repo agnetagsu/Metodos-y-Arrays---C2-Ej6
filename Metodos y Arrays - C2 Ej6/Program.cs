203beb1f5f110ac4a911319a247d39a56ae8b939
Agneta <agnetagsu@yahoo.es>
2021-05-23 11:24:48 +0200

    Agregar archivos de proyecto.

---
+++ Metodos y Arrays - C2 Ej6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Metodos_y_Arrays___C2_Ej6
+{
+    class Cifras
+    {
+        public int calculanumcifras(int numero)
+        {
+            int resultado = numero;
+            int cociente = numero;
+            int suscifras = 0;
+            Boolean fin = false;
+
+            do
+            {
+                resultado = cociente / 10;
+               
+                if (resultado != 0)
+                {
+                    suscifras++;
+                }
+                else
+                {
+                    fin = true;
+                }
+                
+                cociente = resultado;
+            }
+            while (!fin);
+            suscifras++;
+            return suscifras;
+
+        }
+        static void Main(string[] args)
+        {
+            int numero;
+            Cifras numcifras = new Cifras();
+            Console.WriteLine("Introduce un numero:");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(numcifras.calculanumcifras(numero));
+
+        }
+    }
+}
